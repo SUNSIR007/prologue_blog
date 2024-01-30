43bbb6bcc7a4a025c3af7d968b487e5956eec725
Ryuichi <2540987771@qq.com>
2024-01-30 21:21:48 +0800

    修改主页，导航栏

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,5 @@
 const headerNavLinks = [{ href: '/blog', title: 'Blog' },
-    { href: '/booklist', title: 'Booklist' },
 { href: '/about', title: 'About' },
-{ href: '/links', title: 'Links' },]
+]
 
 export default headerNavLinks
